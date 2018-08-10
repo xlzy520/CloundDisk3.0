b72a84ffd722a163f8e7779a8b30f22b272a24b7
zhibi <1017866168@qq.com>
2018-08-10 16:56:20 +0800

    不转成ES5语法

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -55,7 +55,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       favicon: resolve('favicon.ico'),
-      title: 'vue-element-admin'
+      title: '东经云盘'
     }),
   ]
 })
