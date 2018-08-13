3c1fa21c738566c08e98962ee9a22d8dec6d3765
zhibi <1017866168@qq.com>
2018-08-13 15:38:08 +0800

    修改为主页和注销

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -10,11 +10,11 @@
       <el-dropdown-menu class="user-dropdown" slot="dropdown">
         <router-link class="inlineBlock" to="/">
           <el-dropdown-item>
-            Home
+            主页
           </el-dropdown-item>
         </router-link>
         <el-dropdown-item divided>
-          <span @click="logout" style="display:block;">LogOut</span>
+          <span @click="logout" style="display:block;">注销</span>
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
