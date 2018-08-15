b7b985cfd7a87c6e0ff76b9ca41dc1a298cdb95c
zhibi <1017866168@qq.com>
2018-08-15 09:23:25 +0800

    增加宏权的反向代理

--- config/index.js
+++ config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api':{
+        target: 'http://192.168.2.171:8080/',
+        changeOrigin: true,
+        pathRewrite:{'^/api': ''}
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
