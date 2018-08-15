76ca3011be7bf19569499b1551b956ec41e2b7d3
zhibi <1017866168@qq.com>
2018-08-15 10:07:59 +0800

    修改baseURL

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://rap2api.taobao.org/app/mock/24917"',
+  BASE_API: '"api"',
 })
