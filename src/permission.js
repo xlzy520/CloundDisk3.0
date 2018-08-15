f8d7bf13d3d50e1c695a0bddd97a5f98a605dfd3
zhibi <1017866168@qq.com>
2018-08-15 11:17:34 +0800

    注释掉权限判断

--- src/permission.js
+++ src/permission.js
@@ -1,8 +1,8 @@
 import router from './router'
-import store from './store'
+// import store from './store'
 import NProgress from 'nprogress' // Progress 进度条
 import 'nprogress/nprogress.css'// Progress 进度条样式
-import { Message } from 'element-ui'
+// import { Message } from 'element-ui'
 import { getToken } from '@/utils/auth' // 验权
 
 const whiteList = ['/login'] // 不重定向白名单
@@ -13,18 +13,19 @@ router.beforeEach((to, from, next) => {
       next({ path: '/' })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
-      if (store.getters.roles.length === 0) {
-        store.dispatch('GetInfo').then(res => { // 拉取用户信息
-          next()
-        }).catch((err) => {
-          store.dispatch('FedLogOut').then(() => {
-            Message.error(err || 'Verification failed, please login again')
-            next({ path: '/' })
-          })
-        })
-      } else {
-        next()
-      }
+      next()
+      // if (store.getters.roles.length === 0) {
+      //   store.dispatch('GetInfo').then(res => { // 拉取用户信息
+      //     next()
+      //   }).catch((err) => {
+      //     store.dispatch('FedLogOut').then(() => {
+      //       Message.error(err || 'Verification failed, please login again')
+      //       next({ path: '/' })
+      //     })
+      //   })
+      // } else {
+      //   next()
+      // }
     }
   } else {
     if (whiteList.indexOf(to.path) !== -1) {
