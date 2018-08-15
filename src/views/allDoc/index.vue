27fdcca6d0b642d64357d90b60da7d711cd3e15e
zhibi <1017866168@qq.com>
2018-08-15 09:19:35 +0800

    引入markdown编辑器到页面，增加滚动样式，增加height使滚动条出现

--- src/views/allDoc/index.vue
+++ src/views/allDoc/index.vue
@@ -1,13 +1,91 @@
 <template>
-
+  <div class="preview-area">
+    <mavon-editor
+      v-model="value"
+      :toolbars="toolbars"
+      :externalLink="externalLink"
+      codeStyle="monokai-sublime"
+      :scrollStyle="true"
+      class="preview-container"/>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'AllDoc'
-}
+  import '@/styles/markdown.css'
+
+  export default {
+    name: 'AllDoc',
+    data() {
+      return {
+        value: '',
+        externalLink: {
+          hljs_css: function(css) {
+            // 这是你的代码高亮配色文件路径
+            return 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/' + css + '.min.css'
+          },
+          katex_css: false, // `false`表示禁用自动加载，它也可以是个函数，如果它是个函数，那么这个函数应该返回一个可访问的`katex`的css路径字符串
+          // 我们没有设置`katex_js`, `hljs_js`, `hljs_lang`, `markdown_css`， `mavon-editor`会认为它的值为`true`，它会默认使用`cdnjs`相关外链加载
+          katex_js: false,
+          markdown_css: false,
+          hljs_js: function() {
+            // 这是你的hljs文件路径
+            return 'https://cdn.bootcss.com/highlight.js/9.12.0/highlight.min.js'
+          }
+        },
+        toolbars: {
+          bold: true, // 粗体
+          italic: true, // 斜体
+          header: true, // 标题
+          underline: true, // 下划线
+          strikethrough: true, // 中划线
+          mark: true, // 标记
+          superscript: true, // 上角标
+          subscript: true, // 下角标
+          quote: true, // 引用
+          ol: true, // 有序列表
+          ul: true, // 无序列表
+          link: true, // 链接
+          imagelink: true, // 图片链接
+          code: true, // code
+          table: true, // 表格
+          fullscreen: false, // 全屏编辑
+          readmodel: true, // 沉浸式阅读
+          htmlcode: true, // 展示html源码
+          help: true, // 帮助
+          /* 1.3.5 */
+          undo: true, // 上一步
+          redo: true, // 下一步
+          trash: true, // 清空
+          save: true, // 保存（触发events中的save事件）
+          /* 1.4.2 */
+          navigation: true, // 导航目录
+          /* 2.1.8 */
+          alignleft: true, // 左对齐
+          aligncenter: true, // 居中
+          alignright: true, // 右对齐
+          /* 2.2.1 */
+          subfield: true, // 单双栏模式
+          preview: true // 预览
+        }
+      }
+    }
+
+  }
 </script>
 
-<style scoped>
-
+<style>
+  .v-note-wrapper.markdown-body.preview-container{
+    position: absolute;
+    right: 1px;
+    bottom: 1px;
+    width: 60em;
+    height: 85vh;
+  }
+  .v-note-wrapper .v-note-panel .v-note-edit.divarea-wrapper.scroll-style::-webkit-scrollbar-thumb{
+    background-color: #409eff!important;
+  }
+  .v-note-wrapper .v-note-panel .v-note-show .v-show-content.scroll-style::-webkit-scrollbar-thumb,
+  .v-note-wrapper .v-note-panel .v-note-show .v-show-content-html.scroll-style::-webkit-scrollbar-thumb{
+    background-color: #67c23a!important;
+  }
 </style>
