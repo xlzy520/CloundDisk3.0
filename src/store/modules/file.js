08f1c5374270710cb8ab0fab03cbf5a8a3bcf2cd
zhibi <1017866168@qq.com>
2018-08-20 17:01:55 +0800

    上传文件窗口状态

--- src/store/modules/file.js
+++ src/store/modules/file.js
@@ -1,7 +1,22 @@
 const file = {
   state: {
     uploadVisible: false
+  },
+  mutations: {
+    TOGGLE_UPLOADVISIBLE: state => {
+      if (state.uploadVisible) {
+        state.uploadVisible = false
+      } else {
+        state.uploadVisible = true
+      }
+    }
+  },
+  actions: {
+    ToggleUploadVisible: ({ commit }) => {
+      commit('TOGGLE_UPLOADVISIBLE')
+    }
   }
+
 }
 
 export default file
