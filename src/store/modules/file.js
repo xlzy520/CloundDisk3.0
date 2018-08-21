b0ebf34c7cd9f5c47c1f13282e5deed177ec879c
zhibi <1017866168@qq.com>
2018-08-21 10:40:10 +0800

    更新file,增加分类信息

--- src/store/modules/file.js
+++ src/store/modules/file.js
@@ -2,14 +2,14 @@ import { getCategory } from '@/api/file'
 const file = {
   state: {
     uploadVisible: false,
-    list: []
+    fileList: []
   },
   mutations: {
     TOGGLE_UPLOADVISIBLE: state => {
       state.uploadVisible = !state.uploadVisible
     },
     GET_CATEGORY: (state, data) => {
-      state.list = data
+      state.fileList = data
     }
   },
   actions: {
