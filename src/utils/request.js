14777edcbe7b6f2622ce6591ac85c32c66ca4fc1
zhibi <1017866168@qq.com>
2018-08-28 08:47:38 +0800

    移除错误code判断拦截

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Message, MessageBox } from 'element-ui'
+import { Message } from 'element-ui'
 import store from '../store'
 import { getToken } from '@/utils/auth'
 
@@ -32,17 +32,6 @@ service.interceptors.response.use(
         duration: 2 * 1000
       })
       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('FedLogOut').then(() => {
-            location.reload()// 为了重新实例化vue-router对象 避免bug
-          })
-        })
-      }
       return Promise.reject('error')
     } else {
       return response.data
