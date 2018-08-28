57f68dae58e594adb93bbfa898c0a0a86b67c0b8
zhibi <1017866168@qq.com>
2018-08-28 10:50:55 +0800

    判断token过期

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Message } from 'element-ui'
+import { Message, MessageBox } from 'element-ui'
 import store from '../store'
 import { getToken } from '@/utils/auth'
 
@@ -33,6 +33,17 @@ service.interceptors.response.use(
       })
       console.log(1)
       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
+      if (res.code === 100602) {
+        MessageBox.confirm('Token 过期了，您可以取消继续留在该页面，或者重新登录', '确定登出', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('FedLogOut').then(() => {
+            location.reload()// 为了重新实例化vue-router对象 避免bug
+          })
+        })
+      }
       return Promise.reject('error')
     } else {
       return response.data
