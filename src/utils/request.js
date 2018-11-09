e09e230eef48538a0107f23b8b241941c3d053e9
zhibi <1017866168@qq.com>
2018-11-09 10:27:48 +0800

    注释传输自定义token

--- src/utils/request.js
+++ src/utils/request.js
@@ -12,10 +12,10 @@ const service = axios.create({
 
 // request拦截器
 service.interceptors.request.use(config => {
-  if (store.getters.token) {
-    config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
-  }
-  return config
+  // if (store.getters.token) {
+  //   config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+  // }
+   return config
 }, error => {
   // Do something with request error
   console.log(error) // for debug
