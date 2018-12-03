934896b51f323c75302bbb0dbf31d6cfb8859498
alun <314884680@qq.com>
2018-12-03 13:06:16 +0800

    save：保存一下

--- src/components/zipReader.vue
+++ src/components/zipReader.vue
@@ -1,32 +1,25 @@
 <template>
   <div class="zip-reader">
-    <base-dialog ref="dialog"
-                 @close="close"
-                 :title="title">
+    <base-dialog ref="dialog" @close="close" :title="title">
       <div class="zip-reader-content">
         <base-scrollbar>
-          <div class="zip-reader-content-item"
-               v-for="item in zips"
-               @click="clickFile(item)">
+          <div class="zip-reader-content-item" v-for="item in zips" @click="clickFile(item)">
             <div class="zip-reader-content-item-icon">
-              <svg-icon :icon-class="String(item.fileType)"
-                        class-name="icon" />
+              <svg-icon :icon-class="String(item.fileType)" class-name="icon" />
             </div>
-            <div class="zip-reader-content-item-name"
-                 :title="item.simpleName">
+            <div class="zip-reader-content-item-name" :title="item.simpleName">
               {{ item.simpleName }}
+            </div>
+            <div class="zip-reader-content-item-button" v-if="item.fileType !== '1'" @click.stop>
+              <el-button type="primary" size="mini" @click="download(item)">下载</el-button>
             </div>
           </div>
         </base-scrollbar>
       </div>
       <div slot="footer"
            class="zip-reader-footer">
-        <el-button size="small"
-                   type="primary"
-                   @click="back"
-                   v-if="historyNode.length > 1">返 回</el-button>
-        <el-button size="small"
-                   @click="close">关 闭</el-button>
+        <el-button size="small" type="primary" @click="back" v-if="historyNode.length > 1">返 回</el-button>
+        <el-button size="small" @click="close">关 闭</el-button>
       </div>
     </base-dialog>
   </div>
@@ -69,6 +62,9 @@
         this.historyNode.pop();
         this.zips = this.historyNode[this.historyNode.length - 1];
       },
+      download(file) {
+        console.log(file);
+      },
       clickFile(file) {
         if (file.fileType === "1") {
           this.zips = file.children;
@@ -93,36 +89,40 @@
     &-content {
       display: flex;
       position: relative;
+      margin: 10px 0;
+      padding: 0px 20px;
       width: 700px;
-      height: 400px;
+      height: 380px;
       &-item {
-        display: inline-block;
+        position: relative;
         vertical-align: top;
-        margin-left: 20px;
-        margin-top: 20px;
-        width: 120px;
-        height: 150px;
-        text-align: center;
-        border-radius: 6px;
+        margin-top: 10px;
         cursor: pointer;
+        & > div {
+          display: inline-block;
+          vertical-align: middle;
+        }
         &:hover {
           background: #e3ecf76b;
         }
         &-icon {
           margin: auto;
-          padding: 10px;
-          .icon {
-            width: 100px;
-            height: 100px;
-          }
         }
         &-name {
           height: 24px;
           margin-top: 0px;
-          padding: 0 10px;
           overflow: hidden;
           text-overflow: ellipsis;
           white-space: nowrap;
+        }
+        &-button {
+          position: absolute;
+          right: 0;
+          height: 100%;
+          button {
+            margin-top: 1px;
+            padding: 5px 10px;
+          }
         }
       }
     }
