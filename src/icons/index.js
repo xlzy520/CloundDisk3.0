8b6f6a6150e37b8266179343344115917aa54c5d
zhibi <1017866168@qq.com>
2019-01-03 16:57:49 +0800

    svg名字还原

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import SvgIcon from '@/components/SvgIcon/SvgIcon.vue';// svg组件
+import SvgIcon from '@/components/SvgIcon.vue';// svg组件
 
 // register globally
 Vue.component('svg-icon', SvgIcon);
