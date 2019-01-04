abc8216e8d4c123bbf3caddab8ffa1b136754cf3
zhibi <1017866168@qq.com>
2019-01-04 15:24:49 +0800

    增加一秒弹窗全局方法

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,3 +3,10 @@ import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(Element);
+Vue.prototype.$message1000 = (message, type) => { // 默认消息提示持续1S
+  Element.Message({
+    message: message,
+    type: type,
+    duration: 1000
+  });
+};
