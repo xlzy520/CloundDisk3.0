d1e80e1483ef323c40213722fe0b42289232e0da
zhibi <1017866168@qq.com>
2019-01-07 14:37:38 +0800

    优化成字符串模板

--- src/api/base-service/base.js
+++ src/api/base-service/base.js
@@ -1,8 +1,7 @@
 import axios from '@/utils/request';
 
 export default function baseService(url, value, method = 'post', options = {}) {
-  let baseUrl = '/djcpsdocument';
-  let service = baseUrl + url;
+  const service = `/djcpsdocument${url}`;
   if (method === 'post') {
     return axios.post(service, value, options);
   } else if (method === 'get') {
