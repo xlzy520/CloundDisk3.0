3337b0ab856f4d6547d1ecf676fe3e3fdac6e8cd
zhibi <1017866168@qq.com>
2019-01-17 11:31:46 +0800

    重命名

--- src/api/service/dingDingpush.js
+++ src/api/service/dingDingpush.js
@@ -1,7 +1,19 @@
 import service from '../base-service/dingDingPush.js';
 
 export default {
-  getRecycleList(page) {
-    return service('/selectRecycle.do', {pageSize: 20, pageNo: page});
+  getUserInfoByOrgId(keyword) {
+    return service('/getUserInfoByOrgId.do', {...keyword}, 'get');
+  },
+  getRobotList() {
+    return service('/getRobotList.do', {}, 'get');
+  },
+  addFoldPushMan(data) {
+    return service('/addFoldPushMan.do', {...data});
+  },
+  getDingConfigInfo(id) {
+    return service('/getDingConfigInfo.do', {foldId: id}, 'get');
+  },
+  updateFoldPushMan(data) {
+    return service('/updateFoldPushMan.do', {...data});
   },
 };
