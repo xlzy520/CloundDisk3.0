e32fd2382f17cf24dd4eca55e1c4ca9ca8975eb7
Yang <1073406997@qq.com>
2019-03-07 15:10:11 +0800

    和后端协调规定token过期码为100602，一旦token过期清除cookie的token并返回登录页

--- src/utils/request.js
+++ src/utils/request.js
@@ -22,21 +22,12 @@ service.interceptors.response.use(
   response => {
     const res = response.data;
     if (!res.success) {
-      if (res.code === 100602 || res.msg === 'token已过期') { //exchangeToken接口没有code，只有msg
+      if (res.code === "100602" || res.msg === 'token已过期') { //exchangeToken接口没有code，只有msg
         if (router.history.current.path !== '/login') {
-          MessageBox.confirm('Token 过期了，您可以取消继续留在该页面，或者重新登录', '确定登出', {
-            confirmButtonText: '重新登录',
-            cancelButtonText: '取消',
-            type: 'warning',
-            closeOnClickModal: false,
-            showClose: false,
-            roundButton: true,
-            closeOnPressEscape: false
-          }).then(() => {
-            store.dispatch('FedLogOut').then(() => {
-              router.push('/login');
-              //location.reload();// 为了重新实例化vue-router对象 避免bug
-            });
+          removeToken();
+          store.dispatch('FedLogOut').then(() => {
+            router.push('/login');
+            //location.reload();// 为了重新实例化vue-router对象 避免bug
           });
         }
       } else if (res.msg === '120') {
@@ -56,7 +47,7 @@ service.interceptors.response.use(
   error => {
     console.log(error, error.response);// for debug
     error.message = error.message === 'timeout of 5000ms exceeded' ? '连接服务器超时！' : error.message;
-    removeToken()
+    removeToken();
     Message({
       message: error.message || error.msg,
       type: 'error',
