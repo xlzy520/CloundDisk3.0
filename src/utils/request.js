c027508b2bcc2a0770fd9a6b305024a4c6e5696c
Yang <1073406997@qq.com>
2019-03-12 13:52:17 +0800

    request.js 去除一个多余引入 MessageBox

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Message, MessageBox } from 'element-ui';
+import { Message } from 'element-ui';
 import store from '../store';
 import router from '../router';
 import { removeToken } from '@/utils/auth';
