e7ffaf3bdf49ef55e45dc8c1d9bd2a42c496203d
zhibi <1017866168@qq.com>
2019-03-15 17:00:03 +0800

    修复刷新之后没有用户数据的bug

--- src/permission.js
+++ src/permission.js
@@ -1,10 +1,18 @@
 import router from './router';
+import store from './store';
 import NProgress from 'nprogress'; // Progress 进度条
 import 'nprogress/nprogress.css';// Progress 进度条样式
 
 // const whiteList = ['/login', '/index/list']; // 不重定向白名单
 router.beforeEach((to, from, next) => {
   NProgress.start();
+  if (store.getters.userData.name === undefined) {
+    store.dispatch('GetInfo').then(() => { // 拉取用户信息
+      next();
+    }).catch(()=>{
+      next('/login');
+    });
+  }
   next();
 });
 
