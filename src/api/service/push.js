c59801fd49f8c1ca0a1c30ef751522c77f702590
zhibi <1017866168@qq.com>
2019-03-18 16:04:08 +0800

    修改名字

--- src/api/service/push.js
+++ src/api/service/push.js
@@ -2,7 +2,7 @@ import pushService from '../base-service/push.js';
 
 export default {
   // 分享文件弹框获取组织人员
-  getInfoByOrgId (orgId) {
+  getUserInfoByOrgId (orgId) {
     return pushService('/getUserInfoByOrgId.do', { orgId }, 'get');
   },
 };
