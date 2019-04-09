733ef2ecf6ffe617941d590aa410b9282ce04a6c
zhibi <1017866168@qq.com>
2019-04-09 13:30:33 +0800

    用搜索图标占位，点击后显示搜索组件

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -15,26 +15,31 @@
         <router-link to="/index/userManagement" class="nav-router-text auth" v-if="isAdmin">权限管理</router-link>
         <router-link to="/index/record"  v-if="false" class="nav-router-text record">操作记录</router-link>
       </div>
-      <div class="search_wrap"  @focus="showSwitch($event)" @blur="showSwitch($event)">
-        <el-switch
-          v-show="inputHover"
-          class="switch-search"
-          v-model="query.full"
-          active-color="#13ce66"
-          active-text="全文搜索">
-        </el-switch>
-        <md-input
-          icon="search"
-          name="search"
-          @focus="showSwitch"
-          @blur="showSwitch"
-          v-model="query.value"
-          title="请输入搜索关键词"
-          placeholder="请输入关键词，回车搜索"
-          :maxlength="30"
-          :key-enter-function="getSearchResult">
-        </md-input>
+      <div class="search-replace" @click="searchDisplay" v-if="!searchVisible">
+        <i class="fa fa-search search-replace-icon" title="搜索"></i>
       </div>
+      <transition name="slide-fade">
+        <div class="search-wrap" v-if="searchVisible" @focus="showSwitch($event)" @blur="showSwitch($event)">
+          <el-switch
+            v-show="inputHover"
+            class="switch-search"
+            v-model="query.full"
+            active-color="#13ce66"
+            active-text="全文搜索">
+          </el-switch>
+          <md-input
+            icon="search"
+            name="search"
+            @focus="showSwitch"
+            @blur="showSwitch"
+            v-model="query.value"
+            title="请输入搜索关键词"
+            placeholder="请输入关键词，回车搜索"
+            :maxlength="30"
+            :key-enter-function="getSearchResult">
+          </md-input>
+        </div>
+      </transition>
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img class="user-avatar" :src="'https://oa.djcps.com/DJOA/'+userData.userIco">
@@ -57,14 +62,13 @@
         </el-dropdown-menu>
       </el-dropdown>
     </el-menu>
-
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import Hamburger from '@/components/Hamburger.vue';
-import MdInput from '@/components/file/MDinput.vue';
+import MdInput from '@/components/MDinput.vue';
 
 export default {
   name: 'Navbar',
@@ -87,15 +91,19 @@ export default {
         full: false,
         value: ''
       },
-      inputHover: false
+      inputHover: false,
+      searchVisible: false
     };
   },
   methods: {
+    searchDisplay() {
+      this.searchVisible = true;
+    },
     showSwitch(event) {
       if (event.type === 'focus') {
-        this.inputHover = event.target.className === 'material-input' || event.target.className === 'search_wrap';
+        this.inputHover = event.target.className === 'material-input' || event.target.className === 'search-wrap';
       } else {
-        this.inputHover = !(event.target.className === 'material-input' || event.target.className === 'search_wrap');
+        this.inputHover = !(event.target.className === 'material-input' || event.target.className === 'search-wrap');
       }
     },
     toggleSideBar() {
@@ -117,7 +125,7 @@ export default {
 };
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
+<style lang="scss">
 .navbar {
   height: 50px;
   line-height: 50px;
@@ -164,7 +172,20 @@ export default {
       color: #1296db;
     }
   }
-  .search_wrap{
+  .search-replace{
+    position: absolute;
+    right: 10%;
+    cursor: pointer;
+    &-icon{
+      font-size: 24px;
+      height: 50px;
+      line-height: 50px;
+      background-image: -webkit-gradient(linear, left 0, right 0, from(rgb(4, 154, 255)), to(rgb(109, 216, 187)));
+      -webkit-background-clip: text; /*必需加前缀 -webkit- 才支持这个text值 */
+      -webkit-text-fill-color: transparent; /*text-fill-color会覆盖color所定义的字体颜色： */
+    }
+  }
+  .search-wrap{
     position: absolute;
     right: 15%;
     .material-input__component{
@@ -178,15 +199,10 @@ export default {
     }
   }
   .avatar-container {
-    height: 50px;
-    display: inline-block;
     position: absolute;
     right: 35px;
-    top: 0;
     .avatar-wrapper {
       cursor: pointer;
-      margin-top: 5px;
-      position: relative;
       .user-avatar {
         width: 40px;
         height: 40px;
@@ -194,28 +210,19 @@ export default {
       }
       .el-icon-caret-bottom {
         position: absolute;
-        right: -20px;
-        top: 25px;
-        font-size: 12px;
+        top: 12px;
+        font-size: 24px;
+        color: #4cbdf0;
       }
     }
   }
 }
-
-.user-dropdown {
-  line-height: 20px;
-  /deep/ .el-dropdown-menu__item {
-    padding: 0;
-  }
-
-  /deep/ .el-dropdown-menu__item--divided:before {
-    margin: 0;
-  }
-
-  .name {
-    display: block;
-    padding: 0 20px;
-  }
+.slide-fade-enter-active{
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to{
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>
 
